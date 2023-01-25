e0f385b46e5eb329ddb287311895c29ae123a305
rgaliciagg <88796918+rgaliciagg@users.noreply.github.com>
2023-01-25 09:27:43 -0600

    Update rtc_server.js

--- rtc_server.js
+++ rtc_server.js
@@ -3,7 +3,7 @@ const Readline = SerialPort.parsers.Readline;
 //const Readline = SerialPort.parsers.Readline;
 const parser = new Readline();
 const mySerial = new SerialPort('dev/ttyACM0' , {
-    baudRate: 9600
+    baudRate: 9600  //115200 PICO
 });
 
 mySerial.on('open',function(){
